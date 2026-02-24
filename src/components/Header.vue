cf769492714bd3595a1ac065adcb521220a27fc4
Rebecca Smith <rebecca.smith@hotmail.se>
2026-02-24 19:38:15 +0100

    Added HeaderComponent

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <div class="container">
+            <h1>Moment3</h1>
+            <nav>
+                <ul>
+                    <li><RouterLink to ="/">Start</RouterLink></li>
+                    <li><RouterLink to ="/film">Film</RouterLink></li>
+                    <li><RouterLink to ="/info">Info</RouterLink></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script setup>
+    import { RouterLink } from 'vue-router';
+</script>
+
+<style scoped>
+    
+</style>
