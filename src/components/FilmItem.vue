859b265ccadcdf8c84b902de2222c1d31658c1c2
Rebecca Smith <rebecca.smith@hotmail.se>
2026-02-24 21:30:41 +0100

    Added so that the vue-applikations reads data from my api and writes it to the screen

---
+++ src/components/FilmItem.vue
@@ -0,0 +1,9 @@
+<template>
+    <article>{{ film.title }}</article>
+</template>
+
+<script setup>
+    const props = defineProps({
+        film: Object
+    })
+</script>
--- src/views/filmView.vue
+++ src/views/filmView.vue
@@ -1,8 +1,37 @@
 <template>
     <h2>Filmsida</h2>
+    <FilmItem v-for="film in films" :film="film" :key="film._id"/>
 </template>
 
 <script setup>
+    import FilmItem from '@/components/FilmItem.vue';
+    import { ref, onMounted} from 'vue';
+
+    const films =ref([])
+
+    onMounted(() => {
+        console.log("Monuted körs");
+        
+        getFilms();
+    })
+
+
+    const getFilms = async () => {
+
+        console.log("hejhej");
+        
+        try {
+            const res = await fetch("http://127.0.0.1:3000/films")
+            if(res.ok) {
+                const data =await res.json();
+
+                films.value = data;
+            }
+        } catch (error) {
+            console.log("Fel uppstod"+error);
+            
+        }
+    }
 </script>
 
 <style scoped>
