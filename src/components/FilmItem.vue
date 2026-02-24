3a57b744c8209b3a1dc4e34f47562d5ecea127ea
Rebecca Smith <rebecca.smith@hotmail.se>
2026-02-24 23:59:17 +0100

    added function to delete a film

--- src/components/FilmItem.vue
+++ src/components/FilmItem.vue
@@ -1,9 +1,11 @@
 <template>
-    <article>{{ film.title }}</article>
+    <article @click="$emit('deleteFilm', film._id)">{{ film.title }}</article>
 </template>
 
 <script setup>
     const props = defineProps({
         film: Object
     })
+
+    const emits = defineEmits(["deleteFilm"])
 </script>
--- src/views/filmView.vue
+++ src/views/filmView.vue
@@ -1,6 +1,6 @@
 <template>
     <h2>Filmsida</h2>
-    <FilmItem v-for="film in films" :film="film" :key="film._id"/>
+    <FilmItem v-for="film in films" :film="film" :key="film._id" @delete-film="deleteFilm"/>
 </template>
 
 <script setup>
@@ -30,6 +30,28 @@
         } catch (error) {
             console.log("Fel uppstod"+error);
             
+        }
+    }
+
+    const deleteFilm = async (id) => {
+      
+        console.log(id);
+        
+        try {
+              const res = await fetch("http://127.0.0.1:3000/film/"+id, {
+                method: "DELETE"
+
+        });
+                 if(res.ok) {
+
+                    console.log("respons ok");
+                    getFilms();
+                    
+                
+            }
+            
+        } catch (error) {
+            console.log("Fel uppstod"+error);
         }
     }
 </script>
