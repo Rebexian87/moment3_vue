8e04f05827ae95e271136a9db11ef5376d2e7f85
Rebecca Smith <rebecca.smith@hotmail.se>
2026-02-24 19:32:07 +0100

    Created views and routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import filmView from '@/views/filmView.vue'
+import informationView from '@/views/informationView.vue'
+import startView from '@/views/startView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{
+    path: "/",
+    name: "start",
+    component: startView
+  },
+  {
+    path: "/info",
+    name: "information",
+    component: informationView
+   },
+   {
+    path: "/film",
+    name: "film",
+    component: filmView
+  }
+],
 })
 
 export default router
---
+++ src/views/filmView.vue
@@ -0,0 +1,10 @@
+<template>
+    <h2>Filmsida</h2>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
