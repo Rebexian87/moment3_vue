6debb2c827005571687605a9a444527dff10c938
Rebecca Smith <rebecca.smith@hotmail.se>
2026-02-25 20:53:49 +0100

    Added text and banner

--- src/views/informationView.vue
+++ src/views/informationView.vue
@@ -1,5 +1,8 @@
 <template>
-    <h2>Infosida</h2>
+    <h2>Om Vue</h2>
+    <p>Jag tycker att Vue är lätt att sätta sig in i och det har gått smidigt att använda. Jag installerade 
+        Vue genom att köra kommandot npm create vue @latest och under installationen så aktiverade jag SPA. Jag hade gärna
+        jobbat med Vue fler gånger.   </p>
 </template>
 
 <script setup>
--- src/views/startView.vue
+++ src/views/startView.vue
@@ -1,5 +1,7 @@
 <template>
-    <h2>Startsida</h2>
+    <img src="../assets/banner.jpg" alt="">
+    <h2>Filmer- Startsida</h2>
+    <p>Denna sidor visar upp mitt fritidsintresse gällande att se på film. Den visar upp titeln, premiäråret samt om jag har sett filmen eller inte. </p>
 </template>
 
 <script setup>
