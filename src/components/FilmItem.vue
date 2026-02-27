7f860678c946b0355c8d1aed7f94332f8ad44057
Rebecca Smith <rebecca.smith@hotmail.se>
2026-02-27 21:05:40 +0100

    Changed article to table with the movies.

--- src/components/FilmItem.vue
+++ src/components/FilmItem.vue
@@ -1,5 +1,24 @@
 <template>
-    <article @click="$emit('deleteFilm', film._id)">{{ film.title }}</article>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Släppt</th>
+                <th>Sedd</th>
+                <th>Ta bort</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{ film.title }}</td>
+                <td>{{ film.premiereYear }}</td>
+                <td>{{ film.seen ? 'Är sedd': 'Är inte sedd' }}</td>
+                <td @click="$emit('deleteFilm', film._id)">X</td>
+            </tr>
+        </tbody>
+    </table>
+    <!-- <article @click="$emit('deleteFilm', film._id)">{{ film.title }}</article> -->
 </template>
 
 <script setup>
@@ -8,4 +27,9 @@
     })
 
     const emits = defineEmits(["deleteFilm"])
+
+
 </script>
+
+<style>
+</style>
